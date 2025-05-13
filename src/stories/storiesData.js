fb5ce663edf4da0bbc3fdda6dc402a71c6dc5e98
Jin Yang <jinyang@linkedin.com>
2025-05-13 16:54:03 -0700

    data

--- src/stories/storiesData.js
+++ src/stories/storiesData.js
@@ -1,43 +1,477 @@
 const stories = {
   "1": {
     title: "燃烧",
-    date: "01/02/2024",
+    date: "01/22/2023",
     blocks: [
       {
-        image: "https://jinstaticasset.blob.core.windows.net/photos/ranshao_1.jpg",
-        text: "那天阳光很好，你刚好路过我的世界。",
+        image: "https://jinstaticasset.blob.core.windows.net/photos/1_1.jpg",
+        text: "顾飞星, 没有99次了, 这是最后一次了, 抱歉啊, 实现不了你的第三个愿望了, 但是, 我爱你",
       },
       {
-        image: "https://jinstaticasset.blob.core.windows.net/photos/ranshao_2.jpg",
-        text: "你笑的样子，一直留在我脑海。",
+        image: "https://jinstaticasset.blob.core.windows.net/photos/1_2.jpg",
+        text: "我愿意, 我答应你, 你不要离开我好不好, 这已经是第99次了蒋文明, 已经够了, 我愿意! 我愿意!",
       },
     ],
   },
   "2": {
-    title: "一起的旅行",
-    date: "01/02/2024",
+    title: "日日是好日",
+    date: "02/17/2023",
     blocks: [
       {
-        image: "https://jinstaticasset.blob.core.windows.net/photos/ririshihaori_1.jpg",
-        text: "那次出发，是我们最自由的决定。",
+        image: "https://jinstaticasset.blob.core.windows.net/photos/2_1.jpg",
+        text: "我其实想试试那种长达数十年的暗恋. ",
       },
       {
-        image: "https://jinstaticasset.blob.core.windows.net/photos/ririshihaori_2.jpg",
-        text: "那次出发，是我们最自由的决定。啊啊谁打谁都啊谁懂啊,"
+        image: "https://jinstaticasset.blob.core.windows.net/photos/2_2.jpg",
+        text: "如果把我们家的故事写成一本书, 销量一定会很高的."
       },
     ],
   },
   "3": {
-    title: "一起的旅行2",
-    date: "01/02/2024",
+    title: "红豆",
+    date: "03/04/2023",
     blocks: [
       {
-        image: "https://jinstaticasset.blob.core.windows.net/photos/ririshihaori_1.jpg",
-        text: "那次出发，是我们最自由的决定。",
+        image: "https://jinstaticasset.blob.core.windows.net/photos/3_2.jpeg",
+        text: "望舒，镇上的裁缝铺里有我给你准备的嫁衣，若有来世，我当亲手交给你。",
       },
       {
-        image: "https://jinstaticasset.blob.core.windows.net/photos/ririshihaori_2.jpg",
-        text: "那次出发，是我们最自由的决定。啊啊谁打谁都啊谁懂啊,"
+        image: "https://jinstaticasset.blob.core.windows.net/photos/3_1.jpeg",
+        text: "云潜，1952年的冬天好冷，那里没有柿饼，也没有你。这一生，我看过12498次月亮，12498个时刻在思念你。"
+      },
+    ],
+  },
+  "4": {
+    title: "九州四海入境来",
+    date: "03/17/2023",
+    blocks: [
+      {
+        image: "https://jinstaticasset.blob.core.windows.net/photos/4_2.jpeg",
+        text: "一千年了，你还疼不疼？",
+      },
+      {
+        image: "https://jinstaticasset.blob.core.windows.net/photos/4_1.jpeg",
+        text: "千年了，好久不见，我意识到了，终于寻到你了。",
+      },
+    ],
+  },
+  "5": {
+    title: "大漠谣",
+    date: "06/24/2023",
+    blocks: [
+      {
+        image: "https://jinstaticasset.blob.core.windows.net/photos/5_2.jpg",
+        text: "带我回楼兰",
+      },
+      {
+        image: "https://jinstaticasset.blob.core.windows.net/photos/5_1.jpg",
+        text: "做我的王妃",
+      },
+    ],
+  },
+  "6": {
+    title: "苍歧",
+    date: "07/08/2023",
+    blocks: [
+      {
+        image: "https://jinstaticasset.blob.core.windows.net/photos/6_1.jpeg",
+        text: "自幼便为棋子，隐忍数年，未曾想执子的第一步，便是要先学会弃子。",
+      },
+      {
+        image: "https://jinstaticasset.blob.core.windows.net/photos/6_2.jpeg",
+        text: "轩辕明穗，半生沧澜半生封天，没有一刻是自己的。",
+      },
+    ],
+  },
+  "7": {
+    title: "暮色四合",
+    date: "07/23/2023",
+    blocks: [
+      {
+        image: "https://jinstaticasset.blob.core.windows.net/photos/7_2.png",
+        text: "下一站，去西安吧，看了那么多次日落，想看看日出了。",
+      },
+      {
+        image: "https://jinstaticasset.blob.core.windows.net/photos/7_1.jpeg",
+        text: "这最后一站我来陪你去西安，红椿，你别睡着了，你看整片华山都被朝晖点亮，万物复苏。红椿，我爱你。",
+      },
+    ],
+  },
+  "8": {
+    title: "天地藏春",
+    date: "07/28/2023",
+    blocks: [
+      {
+        image: "https://jinstaticasset.blob.core.windows.net/photos/8_1.jpg",
+        text: "一碰面就总要下雨，怕是老天爷不愿见我们在一起。",
+      },
+      {
+        image: "https://jinstaticasset.blob.core.windows.net/photos/8_2.jpg",
+        text: "上天是总想给我一个与你一同打伞的机会。",
+      },
+    ],
+  },
+  "9": {
+    title: "风禾尽起",
+    date: "09/17/2023",
+    blocks: [
+      {
+        image: "https://jinstaticasset.blob.core.windows.net/photos/9_2.JPG",
+        text: "臣张拯，今日死谏，为陛下强留一个盛唐。",
+      },
+      {
+        image: "https://jinstaticasset.blob.core.windows.net/photos/9_1.JPG",
+        text: "往后余生，在下愿与张拯平分阳寿，一生相互。求冥府阴司，文武判官，各路神仙，成全！",
+      },
+    ],
+  },
+  "10": {
+    title: "洗劫伦敦所有的玫瑰",
+    date: "10/07/2023",
+    blocks: [
+      {
+        image: "https://jinstaticasset.blob.core.windows.net/photos/10_1.jpeg",
+        text: "愿见你之前，我接受这一生都要献给上帝的命运。遇见你之后，我向上帝请愿，希望他能给我一副躯体。珂莱蒂，你看见我的眼睛了吗？那里面装着全部的你。",
+      },
+      {
+        image: "https://jinstaticasset.blob.core.windows.net/photos/10_2.jpeg",
+        text: "整个城市都睡着了，我们去散步吧。",
+      },
+    ],
+  },
+  "11": {
+    title: "林记凉茶铺",
+    date: "10/21/2023",
+    blocks: [
+      {
+        image: "https://jinstaticasset.blob.core.windows.net/photos/11_1.jpeg",
+        text: "树枝，你愿意一直被我这样欺负吗？",
+      },
+      {
+        image: "https://jinstaticasset.blob.core.windows.net/photos/11_2.jpeg",
+        text: "心甘，情愿。",
+      },
+    ],
+  },
+  "12": {
+    title: "苍兰诀",
+    date: "10/27/2023",
+    blocks: [
+      {
+        image: "https://jinstaticasset.blob.core.windows.net/photos/12_1.jpeg",
+        text: "他爱她，他不想再欺骗自己。可她这一次却真的不要他了。",
+      },
+      {
+        image: "https://jinstaticasset.blob.core.windows.net/photos/12_2.jpeg",
+        text: "月族人的爱从来都不是负累，不是枷锁。",
+      },
+    ],
+  },
+  "13": {
+    title: "寒门",
+    date: "10/28/2023",
+    blocks: [
+      {
+        image: "https://jinstaticasset.blob.core.windows.net/photos/13_1.jpeg",
+        text: "我向全天下撒下弥天大谎，并非因为我狡诈，而是因为我执着。",
+      },
+      {
+        image: "https://jinstaticasset.blob.core.windows.net/photos/13_2.jpeg",
+        text: "今生灵族灵女，前世寒门第六十七代弟子宣萱，愿为苍生，赴死！",
+      },
+    ],
+  },
+  "14": {
+    title: "梧桐",
+    date: "12/21/2023",
+    blocks: [
+      {
+        image: "https://jinstaticasset.blob.core.windows.net/photos/14_1.jpeg",
+        text: "青云高渺渺，白云澄昭昭———我叫程昭昭",
+      },
+      {
+        image: "https://jinstaticasset.blob.core.windows.net/photos/14_2.jpeg",
+        text: "原来这一世，下一世，都再也没有一个叫昭昭的姑娘，叽叽喳喳在你耳畔笑语了。",
+      },
+    ],
+  },
+  "15": {
+    title: "秦枫颂",
+    date: "12/30/2023",
+    blocks: [
+      {
+        image: "https://jinstaticasset.blob.core.windows.net/photos/15_2.jpeg",
+        text: "不过以己身为棋，供她此生酣畅淋漓，我甘之如饴，九死无悔。",
+      },
+      {
+        image: "https://jinstaticasset.blob.core.windows.net/photos/15_1.jpeg",
+        text: "你从来都是我这一生中，唯一的偏爱。",
+      },
+    ],
+  },
+  "16": {
+    title: "风起时想你",
+    date: "01/13/2024",
+    blocks: [
+      {
+        image: "https://jinstaticasset.blob.core.windows.net/photos/16_1.jpeg",
+        text: "玫瑰花都开了，你怎么还没回来？",
+      },
+      {
+        image: "https://jinstaticasset.blob.core.windows.net/photos/16_2.jpeg",
+        text: "如果有天玫瑰花开了，带着它来看看我好不好。",
+      },
+    ],
+  },
+  "17": {
+    title: "苍歧2-青川",
+    date: "03/10/2024",
+    blocks: [
+      {
+        image: "https://jinstaticasset.blob.core.windows.net/photos/17_1.jpeg",
+        text: "阿礼啊，请替我守护好这片土地，保护好浅浅，这不只是我的青川，也是你的青川。",
+      },
+      {
+        image: "https://jinstaticasset.blob.core.windows.net/photos/17_2.jpeg",
+        text: "青川的公主，永远只做正确的选择，阿礼谨遵公主遗志，还青山一片山和大好，还青川一个海清河晏。",
+      },
+    ],
+  },
+  "18": {
+    title: "河西家万里",
+    date: "04/13/2024",
+    blocks: [
+      {
+        image: "https://jinstaticasset.blob.core.windows.net/photos/18_1.jpeg",
+        text: "沉默，不是因为没有话说，而是有太多的话想说，却不知从何说起。",
+      },
+      {
+        image: "https://jinstaticasset.blob.core.windows.net/photos/18_2.jpeg",
+        text: "我们身上流淌着那些平凡而又伟大的血脉。就像那条红色的河，生生不息，永远向前。",
+      },
+    ],
+  },
+  "19": {
+    title: "大浪",
+    date: "05/01/2024",
+    blocks: [
+      {
+        image: "https://jinstaticasset.blob.core.windows.net/photos/19_1.jpeg",
+        text: "漱石小姐，今天是你诞生的第10亿秒，31岁7个月9小时4分钟20秒，你愿意做我永远的卡戎吗？",
+      },
+      {
+        image: "https://jinstaticasset.blob.core.windows.net/photos/19_2.jpeg",
+        text: "光锥之内，皆是命运。但就算命运注定发生，也不能失去遇见的勇气。",
+      },
+    ],
+  },
+  "20": {
+    title: "叙清梦",
+    date: "06/08/2024",
+    blocks: [
+      {
+        image: "https://jinstaticasset.blob.core.windows.net/photos/20_1.jpeg",
+        text: "你说，你只要权力和地位。其实，我想告诉你，这些远远不够，我还要给你们姐弟一个家。",
+      },
+      {
+        image: "https://jinstaticasset.blob.core.windows.net/photos/20_2.jpeg",
+        text: "事多妨 梦难长 一点深情 半壁斜阳 ",
+      },
+    ],
+  },
+  "21": {
+    title: "南墙2",
+    date: "05/11/2024",
+    blocks: [
+      {
+        image: "https://jinstaticasset.blob.core.windows.net/photos/21_1.jpeg",
+        text: "用十万手无寸铁的百姓，换十万大量精兵，值得。",
+      },
+      {
+        image: "https://jinstaticasset.blob.core.windows.net/photos/21_2.jpeg",
+        text: "姜酒月不要轮回，不要来生，只愿在这血肉筑成的南墙之中，永世禁锢。做一个酒馆的主人——以换三个愿望。一愿十万莲城百姓得以转世重生。二愿所有不甘灵魂，只要来此祈求，撞破南墙，便可更改时空，倒施轮回，令残缺事圆满，使不平者翻天。三愿，虞皇李旭，完成你想要他完成的一切，要他这条千万人换回的贱命，做一个明君。",
+      },
+    ],
+  },
+  "22": {
+    title: "悠悠寸草心",
+    date: "01/20/2024",
+    blocks: [
+      {
+        image: "https://jinstaticasset.blob.core.windows.net/photos/22_1.jpeg",
+        text: "阿兰，我再也追不上你了，所以最后临走时约定的那场比赛，我认输了。这一次换我在你的终点等你。",
+      },
+      {
+        image: "https://jinstaticasset.blob.core.windows.net/photos/22_2.jpeg",
+        text: "赛罕，我终究还是等不到你送给我的春天了。但，心之所向，终至所归。",
+      },
+    ],
+  },
+  "23": {
+    title: "星光电台",
+    date: "03/24/2024",
+    blocks: [
+      {
+        image: "https://jinstaticasset.blob.core.windows.net/photos/23_1.jpeg",
+        text: "做自己的英雄，哪怕只有一天。",
+      },
+      {
+        image: "https://jinstaticasset.blob.core.windows.net/photos/23_2.jpeg",
+        text: "「可乐」和爆米花在「星光」下变成了「香槟」和鱼子酱。",
+      },
+    ],
+  },
+  "24": {
+    title: "归途七万里",
+    date: "03/31/2024",
+    blocks: [
+      {
+        image: "https://jinstaticasset.blob.core.windows.net/photos/24_1.jpeg",
+        text: "金，你才是天空的灯会。",
+      },
+      {
+        image: "https://jinstaticasset.blob.core.windows.net/photos/24_2.jpeg",
+        text: "为之而生，为之荣耀而历经坎坷，为之人权而先丧人权，为之而死，生前掌上千秋，身后碑上无名，悄然无声。",
+      },
+    ],
+  },
+  "25": {
+    title: "蟪蛄春秋",
+    date: "08/03/2024",
+    blocks: [
+      {
+        image: "https://jinstaticasset.blob.core.windows.net/photos/25_1.jpeg",
+        text: "小上官，史书应记你是个明君，不要为我们破例。",
+      },
+      {
+        image: "https://jinstaticasset.blob.core.windows.net/photos/25_2.jpeg",
+        text: "我们都是历史的特例，末代的上官和初代的权臣，史书会歌颂我们的功勋，传唱我们的爱情。",
+      },
+    ],
+  },
+  "26": {
+    title: "水仙",
+    date: "09/08/2024",
+    blocks: [
+      {
+        image: "https://jinstaticasset.blob.core.windows.net/photos/26_1.jpeg",
+        text: "在光芒和废墟里撕裂，在权利和虚无中挣扎。",
+      },
+      {
+        image: "https://jinstaticasset.blob.core.windows.net/photos/26_2.jpeg",
+        text: "我终究还是爱上了镜花水月的卖弄。",
+      },
+    ],
+  },
+  "27": {
+    title: "如果长夜无明",
+    date: "10/11/2024",
+    blocks: [
+      {
+        image: "https://jinstaticasset.blob.core.windows.net/photos/27_2.jpeg",
+        text: "我成为她了，我得到了虚假的你。",
+      },
+      {
+        image: "https://jinstaticasset.blob.core.windows.net/photos/27_1.jpeg",
+        text: "你看，我们比他们，更像他们。",
+      },
+    ],
+  },
+  "28": {
+    title: "流萤",
+    date: "12/15/2024",
+    blocks: [
+      {
+        image: "https://jinstaticasset.blob.core.windows.net/photos/28_1.jpeg",
+        text: "今日过后，咸阳城中的玉面郎君沈鲤，那个她所爱的沈鲤，就将不复存在了。",
+      },
+      {
+        image: "https://jinstaticasset.blob.core.windows.net/photos/28_2.jpeg",
+        text: "牺牲一切达成目的，哪怕是自己也没事。",
+      },
+    ],
+  },
+  "29": {
+    title: "红豆2",
+    date: "12/25/2024",
+    blocks: [
+      {
+        image: "https://jinstaticasset.blob.core.windows.net/photos/29_1.jpeg",
+        text: "你笑了，简砚秋终究是见不得陶仪景淋雨的。",
+      },
+      {
+        image: "https://jinstaticasset.blob.core.windows.net/photos/29_2.jpeg",
+        text: "祝世上所有的简砚秋，事事如意，归途如虹。",
+      },
+    ],
+  },
+  "30": {
+    title: "璃火",
+    date: "01/18/2025",
+    blocks: [
+      {
+        image: "https://jinstaticasset.blob.core.windows.net/photos/30_1.jpeg",
+        text: "玖琰的心中，四界苍生，不抵南黎一人。",
+      },
+      {
+        image: "https://jinstaticasset.blob.core.windows.net/photos/30_2.jpeg",
+        text: "当青鸟叫响整个山谷，你就该知道，有人在等你回家。",
+      },
+    ],
+  },
+  "31": {
+    title: "流氓叙事",
+    date: "02/17/2025",
+    blocks: [
+      {
+        image: "https://jinstaticasset.blob.core.windows.net/photos/31_1.jpeg",
+        text: "我喜欢你，吃黄油多士的你，喝馥芮白的，珠江边东倒西歪的你，榕树下倒在我怀里的你。",
+      },
+      {
+        image: "https://jinstaticasset.blob.core.windows.net/photos/31_2.jpeg",
+        text: "蒋伯驾，广府的台风天过了，可是我的台风天还在，那是我一生都走不出的雨季。",
+      },
+    ],
+  },
+  "32": {
+    title: "归桑梓",
+    date: "03/16/2025",
+    blocks: [
+      {
+        image: "https://jinstaticasset.blob.core.windows.net/photos/32_1.jpeg",
+        text: "612，612，612，思南风，思南风，思南风……",
+      },
+      {
+        image: "https://jinstaticasset.blob.core.windows.net/photos/32_2.jpeg",
+        text: "我是赵南风，听，南风在海螺里。",
+      },
+    ],
+  },
+  "33": {
+    title: "乱春",
+    date: "04/06/2025",
+    blocks: [
+      {
+        image: "https://jinstaticasset.blob.core.windows.net/photos/33_1.jpeg",
+        text: "属于我的那片花海，我早已遇见了。",
+      },
+      {
+        image: "https://jinstaticasset.blob.core.windows.net/photos/33_2.jpeg",
+        text: "你若堕落成魔，我便做这世间最十恶不赦的魔头。这场必死的局，我来和你一同赴死。",
+      },
+    ],
+  },
+  "34": {
+    title: "未逢春时",
+    date: "05/11/2025",
+    blocks: [
+      {
+        image: "https://jinstaticasset.blob.core.windows.net/photos/34_1.jpeg",
+        text: "为何见了本公主不下跪行礼？",
+      },
+      {
+        image: "https://jinstaticasset.blob.core.windows.net/photos/34_2.jpeg",
+        text: "臣韩铮，以乌丸为聘，请凤凰垂幸。",
       },
     ],
   },
